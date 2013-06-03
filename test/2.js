5a4c4e31aff014c44c8d8463a6b20893def75805
Matthew Metnetsky <matthew@cowarthill.com>
2013-06-03 17:53:24 -0400

    2.js :: test for one argument from concat

--- test/2.js
+++ test/2.js
@@ -8,7 +8,7 @@ test('array of strings', function(t) {
     var s = streamify(['1', '2', '3', 'Four']);
 
     s.pipe(concat(function(res) {
-        t.notOk(err, 'No error');
+        t.equal(1, arguments.length, 'concat returns 1 arg');
         t.equal('123Four', res.toString(), 'result matches expectation');
         t.end();
     }));
@@ -18,7 +18,7 @@ test('array of buffers', function(t) {
     var s = streamify([new Buffer('One'), new Buffer('Two')]);
 
     s.pipe(concat(function(res) {
-        t.notOk(err, 'No error');
+        t.equal(1, arguments.length, 'concat returns 1 arg');
         t.equal('OneTwo', res.toString(), 'result matches expectation');
         t.end();
     }));
